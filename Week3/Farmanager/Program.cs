476ee5ccf0310395b0d6745fb1cb5ea9511d5832
Ashkeyev Ali <ashkeyevli@gmail.com>
2019-02-16 03:34:58 +0600

    week3

---
+++ Week3/Farmanager/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarManager2
+{
+    class Layer
+    {
+        private int selectedItem;
+        public int SelectedItem
+        {
+            get
+            {
+                return selectedItem;
+            }
+            set
+            {
+                if (value >= Content.Count)
+                {
+                    selectedItem = 0;
+                }
+                else if (value < 0)
+                {
+                    selectedItem = Content.Count - 1;
+                }
+                else
+                {
+                    selectedItem = value;
+                }
+            }
+        }
+
+
+        public List<FileSystemInfo> Content
+        {
+            get;
+            set;
+        }
+        public void Rename(FileSystemInfo fInfo)//method for renaming
+        {
+            if (fInfo.GetType() == typeof(DirectoryInfo))
+            {
+                DirectoryInfo y = fInfo as DirectoryInfo;
+                for (int i = 1; i <= 2; i++) // creating space for writing name from console
+                {
+                    Console.WriteLine();
+                }
+                for (int i = 0; i < 20; i++)
+                {
+                    Console.Write("  ");
+                }
+                Console.Write("Enter new name:"); //writing in console for user 
+
+                string s = Console.ReadLine(); // name that user enter
+                string path = y.Parent.FullName;
+                string newname = Path.Combine(path, s); // because there is no command in c# for juct renaming name od diredctory
+                y.MoveTo(newname); // we will move file to the same path with new name
+            }
+            else
+            {
+                FileInfo y = fInfo as FileInfo; // sme wirth file
+                for (int i = 1; i <= 2; i++) // creating space for writing name from console
+                {
+                    Console.WriteLine();
+                }
+                for (int i = 0; i < 20; i++)
+                {
+                    Console.Write("  ");
+                }
+                Console.Write("Enter new name:"); //writing in console for user 
+
+                string s = Console.ReadLine();
+                string newname = Path.Combine(y.Directory.FullName, s);
+                y.MoveTo(newname);
+            }
+        }
+
+
+        public void DeleteSelectedItem()
+        {
+            FileSystemInfo fileSystemInfo = Content[selectedItem];
+            if (fileSystemInfo.GetType() == typeof(DirectoryInfo))
+            {
+                Directory.Delete(fileSystemInfo.FullName, true);
+            }
+            else
+            {
+                File.Delete(fileSystemInfo.FullName);
+            }
+            Content.RemoveAt(selectedItem);
+            selectedItem--;
+        }
+        public void Draw()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            for (int i = 0; i < Content.Count; ++i)
+            {
+                if (i == SelectedItem)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.WriteLine((i+1)+". "+Content[i].Name);
+            }
+        }
+    }
+    enum FARMode
+    {
+        DIR,
+        FILE
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FARMode mode = FARMode.DIR;
+            DirectoryInfo root = new DirectoryInfo(@"C:/Users/User/test2");
+            Stack<Layer> history = new Stack<Layer>();
+            history.Push(
+                    new Layer
+                    {
+                        Content = root.GetFileSystemInfos().ToList(),
+                        SelectedItem = 0
+                    }
+                );
+            while (true)
+                            {
+                if (mode == FARMode.DIR)
+                {
+                    history.Peek().Draw();
+                }
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                switch (consoleKeyInfo.Key)
+                {
+                    case ConsoleKey.Delete:
+                        history.Peek().DeleteSelectedItem();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        history.Peek().SelectedItem--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        history.Peek().SelectedItem++;
+                        break;
+                    case ConsoleKey.F2: // console key for renaming
+                        history.Peek().Rename(history.Peek().Content[history.Peek().SelectedItem]);
+                        break;
+                    case ConsoleKey.Backspace:
+                        if (mode == FARMode.DIR)
+                        {
+                            history.Pop();
+                        }
+                        else
+                        {
+                            mode = FARMode.DIR;
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+                    case ConsoleKey.Enter:
+                        int x = history.Peek().SelectedItem;
+                        FileSystemInfo fileSystemInfo = history.Peek().Content[x];
+                        if (fileSystemInfo.GetType() == typeof(DirectoryInfo))
+                        {
+                            DirectoryInfo directoryInfo = fileSystemInfo as DirectoryInfo;
+                            history.Push(
+                               new Layer
+                              {
+                                   Content = directoryInfo.GetFileSystemInfos().ToList(),
+                                   SelectedItem = 0
+                               });
+                        }
+                        else
+                        {
+                            mode = FARMode.FILE;
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            using (StreamReader sr = new StreamReader(fileSystemInfo.FullName))
+                            {
+                                Console.WriteLine(sr.ReadToEnd());
+                            }
+                        }
+                        break;
+
+                }
+            }
+        }
+    }
+}
